b61c38e6a6d95af0500963a4bd1cea0fa310cb8f
Seyoon Jeong <seyoonagain@gmail.com>
2024-10-01 20:06:47 +0900

    Reduce global variables and add error handling for division in calculator

--- script.js
+++ script.js
@@ -1,176 +1,197 @@
 const calculator = document.querySelector('.calculator');
-const calcIcon = document.getElementById('icon');
-const closeButton = document.getElementById('close-button');
-const buttons = document.querySelectorAll('.button');
 const display = document.getElementById('display');
-const dot = document.getElementById('dot');
-const functions = document.querySelectorAll('.function');
-const operators = document.querySelectorAll('.operator');
-let firstOperand, secondOperand, operator, prevOperator, prevOperand;
+let calcState = {
+  firstOperand: null,
+  secondOperand: null,
+  operator: null,
+  prevOperand: null,
+  prevOperator: null,
+};
 let isReadyForNewInput = false;
 
-calcIcon.addEventListener('dblclick', () => {
-  calculator.style.display = 'block';
-});
-closeButton.addEventListener('click', () => {
-  calculator.style.display = 'none';
-  display.textContent = '0';
-  resetOperandsAndOperator();
+document.querySelectorAll('.button').forEach((button) => {
+  button.addEventListener('click', handleButtonClick);
 });
 
-Array.from(buttons)
-  .filter((button) => button.className.match('number'))
-  .forEach((number) =>
-    number.addEventListener('click', () => {
-      if (
-        display.textContent === '0' ||
-        isReadyForNewInput ||
-        display.textContent === 'too long!'
-      ) {
-        display.textContent = limitLengthAndDecimals(number.textContent[0]);
-      } else {
-        display.textContent = limitLengthAndDecimals(
-          display.textContent + number.textContent
-        );
-      }
-    })
-  );
-
-dot.addEventListener('click', () => {
-  if (isReadyForNewInput) {
-    display.textContent = '0.';
-  } else if (display.textContent.indexOf(dot.textContent) === -1) {
-    display.textContent = limitLengthAndDecimals(
-      display.textContent + dot.textContent
-    );
+function handleButtonClick(e) {
+  const classList = [...e.target.classList];
+  if (classList.includes('number')) {
+    handleNumberClick(e.target.textContent);
+  } else if (classList.includes('function')) {
+    handleFunctionClick(e.target.textContent);
+  } else if (classList.includes('operator')) {
+    handleOperatorClick(e.target.textContent);
+  } else if (classList.includes('dot')) {
+    handleDotClick();
+  } else if (classList.includes('equal')) {
+    handleEqualClick();
   }
-});
+}
 
-Array.from(functions)
-  .find((fn) => fn.textContent === 'C')
-  .addEventListener('click', () => {
-    display.textContent = '0';
-    resetOperandsAndOperator();
-  });
+function handleNumberClick(number) {
+  if (
+    getNumberOnDisplay() === '0' ||
+    getNumberOnDisplay() === 'Too long!' ||
+    getNumberOnDisplay() === 'Error' ||
+    isReadyForNewInput
+  ) {
+    setNumberOnDisplay(number[0]);
+  } else {
+    setNumberOnDisplay(getNumberOnDisplay() + number);
+  }
+  isReadyForNewInput = false;
+}
 
-Array.from(functions)
-  .find((fn) => fn.textContent === '±')
-  .addEventListener('click', () => {
-    display.textContent = -1 * display.textContent;
+function handleFunctionClick(fn) {
+  if (fn === 'C') {
+    setNumberOnDisplay('0');
+    resetOperandsAndOperatorToNull();
+    return;
+  }
+  if (fn === '±') {
+    setNumberOnDisplay(-1 * getNumberOnDisplay());
     setOperands();
-  });
-
-Array.from(functions)
-  .find((fn) => fn.textContent === '√')
-  .addEventListener('click', () => {
-    display.textContent = limitLengthAndDecimals(
-      Math.sqrt(display.textContent)
-    );
+    return;
+  }
+  if (fn === '√') {
+    setNumberOnDisplay(Math.sqrt(getNumberOnDisplay()));
     setOperands();
-  });
+    return;
+  }
+  isReadyForNewInput = true;
+}
 
-operators.forEach((button) =>
-  button.addEventListener('click', (e) => {
-    if (!firstOperand || firstOperand === 'too long!')
-      firstOperand = display.textContent;
-    else if (firstOperand && operator) {
-      secondOperand = display.textContent;
-      calculate(firstOperand, secondOperand, operator);
-    }
-    operator = e.target.textContent;
-  })
-);
+function handleOperatorClick(op) {
+  if (
+    !calcState.firstOperand ||
+    calcState.firstOperand === 'Too long!' ||
+    calcState.firstOperand === 'Error'
+  )
+    calcState.firstOperand = getNumberOnDisplay();
+  else if (calcState.firstOperand && calcState.operator) {
+    calcState.secondOperand = getNumberOnDisplay();
+    executeCalculation();
+  }
+  isReadyForNewInput = true;
+  calcState.operator = op;
+}
 
-Array.from(buttons)
-  .find((button) => button.textContent === '=')
-  .addEventListener('click', () => {
-    if (firstOperand && operator) {
-      secondOperand = display.textContent;
-      calculate(firstOperand, secondOperand, operator);
-      return;
-    }
-    if (firstOperand && prevOperand && prevOperator) {
-      calculate(firstOperand, prevOperand, prevOperator);
-      return;
-    }
-  });
+function handleDotClick() {
+  if (isReadyForNewInput) {
+    setNumberOnDisplay('0.');
+  } else if (getNumberOnDisplay().indexOf('.') === -1) {
+    setNumberOnDisplay(getNumberOnDisplay() + '.');
+  }
+  isReadyForNewInput = false;
+}
 
-function calculate(stringX, stringY, op) {
-  const numberX = Number(stringX);
-  const numberY = Number(stringY);
-  let result;
+function handleEqualClick() {
+  if (calcState.firstOperand && calcState.operator) {
+    calcState.secondOperand = getNumberOnDisplay();
+    executeCalculation();
+    return;
+  }
+  if (
+    calcState.firstOperand &&
+    calcState.prevOperand &&
+    calcState.prevOperator
+  ) {
+    executeCalculation(true);
+    return;
+  }
+  isReadyForNewInput = true;
+}
 
+function setNumberOnDisplay(number) {
+  display.textContent = limitLengthAndDecimals(number.toString());
+}
+
+function getNumberOnDisplay() {
+  return display.textContent;
+}
+
+function executeCalculation(usePrev = false) {
+  if (usePrev) {
+    calcState = {
+      ...calcState,
+      secondOperand: calcState.prevOperand,
+      operator: calcState.prevOperator,
+    };
+  }
+  const result = calculate(
+    Number(calcState.firstOperand),
+    Number(calcState.secondOperand),
+    calcState.operator
+  );
+  updateCalculationState(result);
+}
+
+function calculate(x, y, op) {
   switch (op) {
     case '+':
-      result = numberX + numberY;
-      break;
+      return x + y;
     case '-':
-      result = numberX - numberY;
-      break;
+      return x - y;
     case '*':
-      result = numberX * numberY;
-      break;
+      return x * y;
     case '/':
-      result = numberX / numberY;
-      break;
+      return y !== 0 ? x / y : 'Error';
     default:
       console.log('🚨 Invalid operator!');
   }
-
-  prevOperator = op;
-  prevOperand = stringY;
-  display.textContent = limitLengthAndDecimals(result);
-  firstOperand = display.textContent;
-  secondOperand = null;
-  operator = null;
 }
 
-buttons.forEach((button) =>
-  button.addEventListener('click', (e) => {
-    const operatorSymbols = [...operators].map(
-      (operator) => operator.textContent
-    );
-    operatorSymbols.includes(e.target.textContent) ||
-    e.target.textContent === '=' ||
-    e.target.textContent === '±' ||
-    e.target.textContent === '√'
-      ? (isReadyForNewInput = true)
-      : (isReadyForNewInput = false);
-  })
-);
-
-function limitLengthAndDecimals(number) {
-  const result =
-    number.toString().indexOf('.') === -1
-      ? number
-      : number[number.toString().length - 1] === '.'
-      ? number
-      : Math.round(Number(number) * 10000) / 10000;
-
-  if (result.toString().length <= 8) {
-    return result;
-  } else {
-    isReadyForNewInput = true;
-    return 'too long!';
-  }
+function updateCalculationState(result) {
+  setNumberOnDisplay(result);
+  calcState = {
+    ...calcState,
+    firstOperand: getNumberOnDisplay(),
+    secondOperand: null,
+    operator: null,
+    prevOperand: calcState.secondOperand,
+    prevOperator: calcState.operator,
+  };
 }
 
-function resetOperandsAndOperator() {
-  firstOperand = null;
-  secondOperand = null;
-  operator = null;
-  prevOperand = null;
-  prevOperator = null;
+function limitLengthAndDecimals(strNumber) {
+  const roundedOutput =
+    strNumber.includes('.') &&
+    strNumber[strNumber.length - 1] !== '.' &&
+    strNumber.length <= 8
+      ? Math.round(Number(strNumber) * 1000000) / 1000000
+      : strNumber;
+
+  return roundedOutput.toString().length <= 8 ? roundedOutput : 'Too long!';
+}
+
+function resetOperandsAndOperatorToNull() {
+  calcState = {
+    ...calcState,
+    firstOperand: null,
+    secondOperand: null,
+    operator: null,
+  };
 }
 
 function setOperands() {
-  operator
-    ? (secondOperand = display.textContent)
-    : (firstOperand = display.textContent);
+  calcState.operator
+    ? (calcState.secondOperand = getNumberOnDisplay())
+    : (calcState.firstOperand = getNumberOnDisplay());
 }
 
-function displayDateAndTime() {
+document.getElementById('icon').addEventListener('dblclick', () => {
+  calculator.style.display = 'block';
+});
+
+document.getElementById('close-button').addEventListener('click', () => {
+  calculator.style.display = 'none';
+  setNumberOnDisplay('0');
+  resetOperandsAndOperatorToNull();
+});
+
+function getDateAndTime() {
   const dateAndTime = document.getElementById('date-time');
+  const time = document.getElementById('time');
   const date = new Date();
   const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   const months = [
@@ -190,26 +211,15 @@ function displayDateAndTime() {
   const hour = date.getHours();
   const min = date.getMinutes();
   const second = date.getSeconds();
-  const currentTime = `${days[date.getDay()]} 
-  ${months[date.getMonth()]} 
-  ${date.getDate()}${'\xa0\xa0'}
-  ${hour < 10 ? '0' + hour : hour}:${min < 10 ? '0' + min : min}:${
-    second < 10 ? '0' + second : second
-  }`;
-  dateAndTime.textContent = currentTime;
-}
-
-function displayTime() {
-  const time = document.getElementById('time');
-  const date = new Date();
-  const hour = date.getHours();
-  const min = date.getMinutes();
   const currentTime = `
   ${hour < 10 ? '0' + hour : hour}:${min < 10 ? '0' + min : min}`;
+  const currentDateAndTime = `${days[date.getDay()]} 
+  ${months[date.getMonth()]} 
+  ${date.getDate()}${'\xa0\xa0'}
+  ${currentTime}:${second < 10 ? '0' + second : second}`;
+  dateAndTime.textContent = currentDateAndTime;
   time.textContent = currentTime;
 }
 
-displayDateAndTime();
-displayTime();
-setInterval(() => displayDateAndTime(), 1000);
-setInterval(() => displayTime(), 1000 * 10);
+getDateAndTime();
+setInterval(() => getDateAndTime(), 1000);
